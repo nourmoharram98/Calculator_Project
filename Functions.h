23be6283b094a8d8f15bda6687c5a760483afbba
nourmoharram98 <nourmoharram998@gmail.com>
2023-11-16 23:11:39 +0200

    second commit

---
+++ Functions.h
@@ -0,0 +1,49 @@
+
+//ADD Header - Moamen
+
+
+
+
+
+
+
+
+
+
+
+//SUB Header - Sara
+
+
+
+
+
+
+
+
+
+// MUL Header  - Asmaa
+
+
+
+
+
+
+
+
+
+
+
+//Divide Header  - Nour
+
+
+
+
+
+
+
+
+
+
+
+
+//Modulus Header   - Karim
---
+++ main.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include"Functions.h"
+
+
+
+
+int main(void)
+{
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+//ADD Implementation - Moamen
+
+
+
+
+
+
+
+
+
+
+
+//SUB Implementation - Sara
+
+
+
+
+
+
+
+
+
+// MUL Implementation  - Asmaa
+
+
+
+
+
+
+
+
+
+
+
+//Divide Implementation  - Nour
+
+
+
+
+
+
+
+
+
+
+
+
+//Modulus Implementation   - Karim
